9c824ae4ced319fc9f3692f6f2a58465c4fbefe1
Garwxn <53001540+Garwxn@users.noreply.github.com>
2020-02-14 17:08:36 +0300

    add LCD folder

---
+++ LCD/LCD.ino
@@ -0,0 +1,49 @@
+#include <LiquidCrystal.h>
+LiquidCrystal LCD(10,9,5,4,3,2);
+
+int trigPin=13;
+int echoPin=11;
+float pingTime;
+float targetDistance;
+float speedofSound=776.5;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  LCD.begin(16,2);
+ 
+
+   
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2000);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(15);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(10);
+
+  pingTime = pulseIn(echoPin, HIGH);
+  pingTime = pingTime/1000000.;
+  pingTime = pingTime/3600.;
+  targetDistance = speedofSound*pingTime;
+  targetDistance = targetDistance/2;
+  targetDistance = targetDistance*160934;
+
+  LCD.setCursor(0,0);
+  LCD.print("Target Distance: ");
+  LCD.setCursor(0,1);
+  LCD.print(targetDistance);
+  LCD.print(" cm"); 
+  delay(250);
+
+
+
+}
